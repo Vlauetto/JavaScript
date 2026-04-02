faee9a812e88eddfcb58b0fa0a9abb528603df91
Vlauetto <vlauetto@gmail.com>
2026-04-01 23:35:43 -0300

    aula 4

---
+++ javascript/elementos.js
@@ -0,0 +1,13 @@
+function verde()
+{
+    document.querySelector(".fundo").style.backgroundColor="green";
+     document.querySelector(".fundo").style.color="white";
+     document.querySelector(".myImage").src="../imagens/flor-unica-de-margarida-amarela-fresca-fecha-a-beleza-gerada-por-ia_188544-15543.avif"
+}
+
+function azul()
+{
+    document.querySelector(".fundo").style.backgroundColor="blue";
+     document.querySelector(".fundo").style.color="white";
+     document.querySelector(".myImage").src="../imagens/R.jpg"
+}
